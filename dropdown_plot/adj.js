93d975207ea5d1ba02927d9ba70f0a3881cbc8dc
Jazmin Hicks <jazmin.friashicks@gmail.com>
2019-09-24 18:48:07 -0500

    single dropdown adj change

--- dropdown_plot/adj.js
+++ dropdown_plot/adj.js
@@ -1,48 +1,76 @@
+d3.json("Summ_adj.json").then(function(defaultData) {
+    let dictdefault = defaultData[0];
+    let defaultX = [];
+    let defaultY = [];
+    console.log(dictdefault);
+    Object.entries(dictdefault).forEach(([key, value]) => {
+        if (key.includes("ethnicity")) {
+            defaultX.push(key);
+            defaultY.push(value);
+        }
+    })
+    let defaultBar = [{
+        x:defaultX,
+        y:defaultY,
+        type: 'bar'
+    }];
 
-function filterTable(word) {
+    let defaultLayout = {
+        title : "DataMate Query Results"
+    }
+
+    Plotly.newPlot("plot", defaultBar, defaultLayout);
+})
+
+    
+
+
+
+function filterTableadj(word) {
     d3.json("Summ_adj.json").then(function(adjData) {
         console.log(adjData);
         adjData.forEach(function(data) {
             Object.entries(data).forEach(([key, value]) => {
                 if (key === "adj" & value === word) {
                     console.log(data);
-                    //buildPlots(data);
-                    //return data;
-                }
+                    buildPlots(data);
+                } 
             })
         })
-    })
 
+
+    })
 }
 
 
-
-/*function adjectiveChange(word) {
-    console.log(word);
-    filterTable(word);
-
+function adjectiveChange(word) {
+    filterTableadj(word);
     return word;
 }
 
 function buildPlots(data) {
     
-    arrEthnicity = [];
-    arrEthcount = [];
+    let arrEthnicity = [];
+    let arrcount = [];
     Object.entries(data).forEach(([key, value]) => {
         if (key.includes("ethnicity")) {
             arrEthnicity.push(key);
-            arrEthcount.push(parseInt(value));
+            arrcount.push(parseInt(value));
             //console.log(key);
         }
     })
-    console.log(arrEthcount);
-    console.log(arrEthnicity);
+    //console.log(arrcount);
+    //console.log(arrEthnicity);
 
     ethnicityData = [{
-        y : arrEthcount,
+        y : arrcount,
         x : arrEthnicity,
         type: 'bar'
     }];
 
-    Plotly.react("bar", ethnicityData);
-}*/
+    let layout = {
+        title: "DataMate Query Results"
+    }
+
+    Plotly.react("plot", ethnicityData, layout);
+}
