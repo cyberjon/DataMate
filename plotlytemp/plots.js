2ae3a271e9feae7e0f1b1efca88ea20921531d1c
Ezra Kenigsberg <acumen@gmail.com>
2019-09-24 01:57:12 -0500

    all data files now being created

--- plotlytemp/plots.js
+++ plotlytemp/plots.js
@@ -1,12 +1,18 @@
-// set up arrays of object labels
+// set up arrays of object keys
 let arrDimAge = ["ageLT20", "age2029", "age3039", "age4049", "age5059", "age6069", "age7079", "age8089", "age90"];
 let arrDimEth = ["ethnicityasian", "ethnicityblack", "ethnicitycaucasian", "ethnicityhispanic", "ethnicityindian", 
 "ethnicitymiddleeastern", "ethnicitymixedrace", "ethnicitynativeamerican", "ethnicityotherethnicity"];
 let arrDimHair = ["hairbald", "hairblack", "hairblond", "hairbrown", "hairgrey", "hairmixedcolor", "hairred", "hairblank"];
 
+// set up arrays of object labels
+let arrLblAge = ["<20", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90+"];
+let arrLblEth = ["Asian", "Black", "Caucasian", "Hispanic", "Indian", 
+"Middle Eastern", "Mixed Race", "Native American", "Other Ethnicity"];
+let arrLblHair = ["Bald", "Black", "Blond(e)", "Brown", "Grey", "Mixed Color", "Red", "(Blank)"];
+
 // assign chosen variables
-let strChosenAdj = "honest";
-let arrX = arrDimEth;
+let strChosenAdj = "sexy";
+let arrX = arrDimAge;
 
 // populate arrays to be used by graph
 let arrY = [];
@@ -16,13 +22,19 @@ arrData.forEach(objRow=>{
 	}
 });
 
+let arrLbl = [];
+if(arrX == arrDimAge){arrLbl = arrLblAge;}
+if(arrX == arrDimEth){arrLbl = arrLblEth;}
+if(arrX == arrDimHair){arrLbl = arrLblHair;}
+console.log('arrLbl: ' + arrLbl);
+
 // debugs
 console.log('arrChosenDim: ' + arrX);
 console.log('arrY: ' + arrY);
 
 // set trace var
 let objSettings = {
-  x: arrX,
+  x: arrLbl,
   y: arrY,
   name: "Bucket",
   type: "bar",
